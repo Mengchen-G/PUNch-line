8d08231c4ae92e96183fabd64d0c396623db350c
Caleb Cyphers <caleb.j.cyphers@gmail.com>
2021-03-21 00:03:03 -0600

    Refactor random joke into seperate component

--- joke-app/src/App/App.js
+++ joke-app/src/App/App.js
@@ -1,27 +1,8 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
-import { useState, useEffect } from "react";
-import { getJoke } from "../apiCalls";
+import HomePage from "../Components/HomePage/HomePage";
 
 function App() {
-  const [randomJoke, setRandomJoke] = useState({});
-  const loadNewJoke = () => {
-    getJoke()
-      .then((res) => setRandomJoke(res.data))
-      .catch(console.log("something went wrong"));
-  };
-  useEffect(() => {
-    loadNewJoke();
-  }, []);
-
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-      {randomJoke.punchline && <h2>{randomJoke.setup}</h2>}
-      <br />
-      {randomJoke.punchline && <h2>{randomJoke.punchline}</h2>}
-    </div>
-  );
+  return <HomePage />;
 }
 
 export default App;
---
+++ joke-app/src/Components/HomePage/HomePage.js
@@ -0,0 +1,28 @@
+import "./HomePage.css";
+import { useState, useEffect } from "react";
+import { getJoke } from "../../apiCalls";
+
+function HomePage() {
+  const [randomJoke, setRandomJoke] = useState({});
+  const [error, setError] = useState("");
+  const loadNewJoke = () => {
+    getJoke()
+      .then((res) => setRandomJoke(res.data))
+      .catch((err) => setError(err));
+  };
+  useEffect(() => {
+    loadNewJoke();
+  }, []);
+
+  return (
+    <div className="HomePage">
+      <h1>Hello World</h1>
+      {error && <p>Sorry, there was a problem loading the joke</p>}
+      {randomJoke.punchline && <h2>{randomJoke.setup}</h2>}
+      <br />
+      {randomJoke.punchline && <h2>{randomJoke.punchline}</h2>}
+    </div>
+  );
+}
+
+export default HomePage;
